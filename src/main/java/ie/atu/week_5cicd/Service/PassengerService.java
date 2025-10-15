8ef1a8dbd099b2a6fbe8f3ed4887f95d5c894382
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2025-10-15 14:49:18 +0100

    Added to Passenger Service class

--- src/main/java/ie/atu/week_5cicd/Service/PassengerService.java
+++ src/main/java/ie/atu/week_5cicd/Service/PassengerService.java
@@ -1,4 +1,38 @@
 package ie.atu.week_5cicd.Service;
 
+import ie.atu.week_5cicd.Model.Passenger;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class PassengerService {
+    private final List<Passenger> store = new ArrayList<>();
+
+    public List<Passenger> findALL() {
+        return new ArrayList<>(store);
+    }
+
+    public Optional<Passenger> findById(String id) {
+        for (Passenger p : store) {
+            if (p.getPassengerID().equals(id)) {
+                return Optional.of(p);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Passenger create(Passenger p) {
+        if (findById(p.getPassengerID()).isPresent()) {
+            throw new IllegalArgumentException("Passenger already exists");
+        }
+        store.add(p);
+        return p;
+    }
+
+
+
 }
